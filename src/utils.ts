33e68f2d0c611be3c7168985097def85acbc260d
Jelilat Anofiu <anofiujelailah@gmail.com>
2024-02-04 15:10:47 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export const timeValid = (timestamp: number): boolean => {
   // Farcaster epoch starts on Jan 1, 2021
   const farcasterEpochStart = new Date("2021-01-01T00:00:00Z").getTime() / 1000;
   const currentTimestamp = Math.floor(Date.now() / 1000) - farcasterEpochStart; // Current time in seconds since Farcaster epoch
-  const thirtyMinutes = 3000 * 60; // 30 minutes in seconds
+  const thirtyMinutes = 30 * 60; // 30 minutes in seconds
 
   return currentTimestamp - timestamp <= thirtyMinutes;
 };
